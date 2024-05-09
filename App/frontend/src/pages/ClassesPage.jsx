89345e00483a155330dbb1bad378683a84ba8b95
Maria Ramirez <ramirma2@classwork.engr.oregonstate.edu>
2024-05-08 20:33:13 -0700

    added tables and forms for Classes and Schedules

--- App/frontend/src/pages/ClassesPage.jsx
+++ App/frontend/src/pages/ClassesPage.jsx
@@ -1,11 +1,69 @@
-
+import {useState} from 'react';
 
 function ClassesPage(){
 
-return(
-    <>
-    </>
-)
+    const [showAddClass, setShowAddClass] = useState(false);
+    const [name, setName] = useState("");
+    const [duration, setDuration] = useState("");
+    const [capacity, setCapacity] = useState("");
+    const [description, setDescription] = useState("");
+
+    return(
+        <section>
+            <div>
+                <h2>Manage classes offered</h2>
+
+
+                <table className="classes-list">
+                    <tr>
+                        <th>Class Name</th>
+                        <th>Duration in minutes</th>
+                        <th>Capacity</th>
+                        <th>Description</th>
+                        <th>Edit Class</th>
+                        <th>Delete Class</th>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </table>
+            </div>
+
+            
+            <form>
+                <label>Class Name:</label>
+                <input type="text" value={name} 
+                onChange={e=> setName(e.target.value)} />
+                <label>Duration in minutes:</label>
+                <input type="text" value={duration} 
+                onChange={e=> setDuration(e.target.value)} />
+                <label>Capacity:</label>
+                <input type="text" value={capacity} 
+                onChange={e=> setCapacity(e.target.value)} />
+                <label>Description:</label>
+                <input type="textarea" value={description} 
+                onChange={e=> setDescription(e.target.value)} />
+                <button
+                    onClick={e=>{
+                        e.preventDefault();
+                    }}
+                >Add New Class</button>
+            </form>
+        </section>
+    )
 }
 
 export default ClassesPage
--- App/frontend/src/pages/SchedulesPage.jsx
+++ App/frontend/src/pages/SchedulesPage.jsx
@@ -1,9 +1,87 @@
-
+import {useState} from 'react';
 
 function SchedulesPage(){
+
+    const [className, setClassName]=useState("");
+    const [date, setDate]=useState("");
+    const [dayOfTheWeek, setDayOfTheWeek]=useState("");
+    const [startTime, setStartTime]=useState("");
+    const [endTime, setEndTime]=useState("");
+    const [instructorName, setInstructorName]=useState("");
+    const [status, setStatus]=useState("");
+
     return(
-        <>
-        </>
+        <section>
+            <div>
+                <h2>Manage our classes schedule</h2>
+
+
+                <table className="schedule-list">
+                    <tr>
+                        <th>Class Name</th>
+                        <th>Date</th>
+                        <th>Day of the Week</th>
+                        <th>Start Time</th>
+                        <th>End Time</th>
+                        <th>Instructor</th>
+                        <th>Current Status</th>
+                        <th>Edit Schedule</th>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </table>
+            </div>
+
+            
+            <form>
+                <label>Class Name:</label>
+                <input type="text" value={className} 
+                onChange={e=> setClassName(e.target.value)} />
+                <label>Date:</label>
+                <input type="date" value={date} 
+                onChange={e=> setDate(e.target.value)} />
+                <label>Day of the Week:</label>
+                <input type="text" value={dayOfTheWeek} 
+                onChange={e=> setDayOfTheWeek(e.target.value)} />
+                <label>Start Time:</label>
+                <input type="datetime" value={startTime} 
+                onChange={e=> setStartTime(e.target.value)} />
+                <label>End Time:</label>
+                <input type="datetime" value={endTime} 
+                onChange={e=> setEndTime(e.target.value)} />
+                <label>Instructor:</label>
+                <select
+                onChange= {e=> setInstructorName(e.target.value)}>
+                    <option>Johnny</option>
+                    <option>Lucas</option>
+                    <option>Jess</option>
+                    <option>Kailee</option>
+                </select>
+                <button
+                    onClick={e=>{
+                        e.preventDefault();
+                    }}
+                >Schedule Class</button>
+            </form>
+        </section>
     )
 
 }
